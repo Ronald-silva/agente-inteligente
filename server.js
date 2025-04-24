f1acace1f1bf52f7cf71c9c223c3e404ad60644d
Ronald-silva <ronaldsilvajob@yahoo.com>
2025-04-24 14:04:17 -0300

    feat: adiciona dashboard de teste

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const webhook = require('./controllers/webhook');
 
 dotenv.config();
@@ -7,10 +8,17 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Rota para verificar se está online
 app.get('/', (req, res) => {
   res.send('🤖 Carla - LuceBot está online!');
 });
 
+// Rota do dashboard
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+
+// Rota do webhook
 app.post('/webhook', webhook);
 
 const PORT = process.env.PORT || 3000;
